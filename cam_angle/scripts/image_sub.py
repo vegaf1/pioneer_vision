8dbebc4560b96946e54f80b6b84573ce0582732a
Santosh Balajee Banisetty <santoshbanisetty@nevada.unr.edu>
2019-07-16 14:35:18 -0700

    Code clean up; Removed unwanted code

--- cam_angle/scripts/image_sub.py
+++ cam_angle/scripts/image_sub.py
@@ -24,68 +24,38 @@ pi = 3.14159
 class image_converter:
 
     def __init__(self):
-        self.image_pub = rospy.Publisher("image_topic_2",Image,1000)
+        self.image_pub = rospy.Publisher("image_topic_2",Image,queue_size=1000)
 
         self.bridge = CvBridge()
         self.image_sub = rospy.Subscriber("/usb_cam/image_raw",Image,self.callback)
-
+    # Image callback to convert to ros compatible image, 
     def callback(self,data):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
             print(e)
 
-        (rows,cols,channels) = cv_image.shape
-        if cols > 60 and rows > 60 :
-            cv2.circle(cv_image, (50,50), 10, 255)
-
-        cv2.imshow("Image window", cv_image)
-        cv2.waitKey(3)
-
         try:
             self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
         except CvBridgeError as e:
             print(e)
 
-def main(args):
-    ic = image_converter()
-    rospy.init_node('image_converter', anonymous=True)
-    #pixel_server()
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
-    cv2.destroyAllWindows()
-
+#Angle computation function
 def cam_angle(req):
-
-
     f = (center_x)/(math.tan(0.61453)) #decimal is half of the horizontal fov of camera
-
     center = np.array([0,f])
-
     pixel = np.array([abs(req.x-center_x), f])
-
     dot = np.dot(center,pixel)  #dot product between two vectors
-
-
-
     w = np.linalg.norm(center) #magnitude of the vectors
-
-
     z = np.linalg.norm(pixel)
-
-
-
     angle = math.acos(dot/(w*z)) #dot product formula
-
     deg_angle = angle * 180 / pi 
-
     print ("[x: %s   y: %s]"%(req.x, req.y))
 
     return pixelResponse(deg_angle)
 
 def pixel_server():
+    ic = image_converter()
     rospy.init_node('pixel_server')
     s = rospy.Service('pixel_server',pixel, cam_angle)
     print ('Ready to display angle')
@@ -93,5 +63,3 @@ def pixel_server():
 
 if __name__ == '__main__':
     pixel_server()
-    main(sys.argv)
- 
